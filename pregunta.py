31096fae35949ba45fc875ed19229f3fc10d1035
acastrillonv <48304499+acastrillonv@users.noreply.github.com>
2022-11-26 09:49:23 -0500

    Primer intento

--- pregunta.py
+++ pregunta.py
@@ -18,4 +18,30 @@ def ingest_data():
     # Inserte su código aquí
     #
 
+    df= pd.read_fwf("clusters_report.txt",widths=[9, 16, 16, 80],header=None)
+    list_col = df[:2].fillna('').apply(lambda x: ' ' + x).sum().tolist()
+    list_col = [col.strip().lower().replace(' ', '_') for col in list_col]
+
+    df = df[3:]
+    df.columns = list_col
+    df = df.fillna(method = 'ffill')
+        
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(
+        lambda palabras: ' ' + palabras
+    )
+        
+    df = df.groupby(['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave'], as_index=False)[['principales_palabras_clave']].sum()
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].astype(int)
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace(',', '.').apply(lambda x: float(x[:-2]))
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace(".", "", regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("   "," ")
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("  "," ")
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("  "," ")
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.strip()
+
+
+    df.cluster = df.cluster.astype(int)
+    df = df.sort_values('cluster')
+    df = df.reset_index(drop=True)
+
     return df
